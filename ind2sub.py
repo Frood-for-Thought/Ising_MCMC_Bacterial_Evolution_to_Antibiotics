db399ff40293edf062ad4228dafd1be711334e9b
Michael Frood <132622476+Frood-for-Thought@users.noreply.github.com>
2023-08-28 16:45:45 -0400

    Create ind2sub.py

---
+++ ind2sub.py
@@ -0,0 +1,11 @@
+# This replaces the MATLAB built-in function: [row, col] = ind2sub(array, rand_element_selected)
+# Given the array and the random element number in the array, find the row and column for said element
+# the elements are numbered from left to right, top to bottom
+def fnd_pos(array, pos):
+    col = pos - 1  # start at column 0
+    for i, j in enumerate(array):
+        col -= len(j)
+        if col < 0:
+            col += len(j)
+            row = i
+            return row, col
